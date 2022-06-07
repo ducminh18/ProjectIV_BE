4fad4244c85a9937b0a761533e28f73f7f460d8d
pdong2001 <phamphuongdong2001@gmail.com>
2022-06-07 22:48:11 +0700

    commit

--- public/assets/home/js/homeController.js
+++ public/assets/home/js/homeController.js
@@ -1,7 +1,7 @@
 const route = "products";
 extendController = function ($scope, $http) {
     $scope.page = 1;
-    $scope.limit = 4;
+    $scope.limit = 8;
     $scope.column = "created_at";
     $scope.sort = "desc";
     $scope.extendQuerys = "visible_only=true&consumable_only=true";
