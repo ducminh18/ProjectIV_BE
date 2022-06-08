360bb81a890de0571e08a0105d8d746057f2c1de
NguyenMinhDuc <ducnm1802@gmail.com>
2022-06-08 17:24:02 +0700

    commit

--- resources/views/home/partial/slider.blade.php
+++ resources/views/home/partial/slider.blade.php
@@ -2,12 +2,12 @@
 <section class="section-slide">
     <div class="wrap-slick1">
         <div class="slick1">
-            <div class="item-slick1" style="background-image: url(/assets/home/images/slide-01.jpg);">
+            <div class="item-slick1" style="background-image: url(/assets/home/images/slide_april-collection.jpg);">
                 <div class="container h-full">
                     <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                         <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
                             <span class="ltext-101 cl2 respon2">
-                                Bộ Sưu Tập Nữ 2022
+                                Bộ Sưu Tập Nam 2022
                             </span>
                         </div>
 
@@ -18,7 +18,7 @@
                         </div>
 
                         <div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
-                            <a href="/assets/home/product.html"
+                            <a href="/products"
                                 class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
                                 Xem ngay
                             </a>
@@ -27,55 +27,26 @@
                 </div>
             </div>
 
-            <div class="item-slick1" style="background-image: url(/assets/home/images/slide-02.jpg);">
+            <div class="item-slick1" style="background-image: url(/assets/home/images/slide_april-sale.jpg);">
                 <div class="container h-full">
                     <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                         <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
                             <span class="ltext-101 cl2 respon2">
-                                Thời trang nam mùa mới
+                                
                             </span>
                         </div>
 
                         <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
                             <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                Jackets & Coats
-                            </h2>
+                                
                         </div>
 
                         <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-                            <a href="/assets/home/product.html"
-                                class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                                Xem ngay
-                            </a>
+                            
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <div class="item-slick1" style="background-image: url(/assets/home/images/slide-03.jpg);">
-                <div class="container h-full">
-                    <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
-                        <div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
-                            <span class="ltext-101 cl2 respon2">
-                                Bộ Sưu Tập Nam 2022
-                            </span>
-                        </div>
-
-                        <div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
-                            <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-                                MỚI RA MẮT
-                            </h2>
-                        </div>
-
-                        <div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
-                            <a href="/assets/home/product.html"
-                                class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-                                Xem ngay
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </div>                       
         </div>
     </div>
 </section>
